f97e436d91c8e8336fe760bfd6f45c106a0ed9ec
Filipp Fediakov <fediakovphilip@gmail.com>
2023-06-05 09:03:48 +0100

    Add prettier and eslint

--- test/aws-cdk-multi-tenant-api-example.test.ts
+++ test/aws-cdk-multi-tenant-api-example.test.ts
@@ -5,13 +5,12 @@
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/aws-cdk-multi-tenant-api-example-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AwsCdkMultiTenantApiExample.AwsCdkMultiTenantApiExampleStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new AwsCdkMultiTenantApiExample.AwsCdkMultiTenantApiExampleStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
 });
